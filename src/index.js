e0fb1a3d95e2a78159266b9434eba48e43bab4f4
Timon <timonvanwaardhuizen@gmail.com>
2021-05-11 22:22:07 +0200

    Add Styletron engine

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
+const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+// Create client engine instance
+const engine = new Styletron();
+
+// Provide the engine to the app (debug engine needs inlined source maps)
 ReactDOM.render(
-  <React.StrictMode>
+  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
     <App />
-  </React.StrictMode>,
+  </StyletronProvider>,
   document.getElementById('root')
 );
 
