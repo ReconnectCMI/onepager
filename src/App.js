3ccafcf4042c2ad5bd3a03639e2437dab8864a18
miospruit <mio12@hotmail.nl>
2021-06-03 18:09:41 +0200

    Fix footer and nav.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Container } from "atomize";
+import { Container, Div } from "atomize";
 import 'materialize-css';
 import { Navbar, Footer } from 'react-materialize';
 import Team from './components/team'
@@ -9,21 +9,23 @@ import React from 'react';
 
 function App() {
   return (
+  <Div>
+    <Navbar centerLogo>
+      <a href="#" className="brand-logo center">Logo</a>
+      <ul>
+        <li><a href="#Team">Team</a></li>
+        <li><a href="#Concepten">Concepten</a></li>
+        <li><a href="#Stappen">Stappen</a></li>
+        <li><a href="#Proces">Proces</a></li>
+    </ul>
+    </Navbar>
     <Container className="App">
-      <Navbar centerLogo>
-        <a href="#" className="brand-logo center">Logo</a>
-        <ul>
-          <li><a href="#Team">Team</a></li>
-          <li><a href="#Concepten">Concepten</a></li>
-          <li><a href="#Stappen">Stappen</a></li>
-          <li><a href="#Proces">Proces</a></li>
-        </ul>
-      </Navbar>
       <Banner/>
       <Team/>
       <Project/>
-      <Footer copyrights="© 2021 Reconnect" />
     </Container>
+    <Footer copyrights="© 2021 Reconnect" />
+    </Div>
   );
 }
 
--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'materialize-css';
+import { Div } from "atomize";
 import {Tab, Tabs} from 'react-materialize';
 
 import Connect from "./connect"
@@ -9,11 +10,14 @@ import Create from "./create"
 class Project extends React.Component {
     render() {
         return (
+            <Div>
+                {/* <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/> */}
             <Tabs className='tab-demo z-depth-1'>
                 <Tab title="Connect"active><Connect/></Tab>
                 <Tab title="Concept"><Concept/></Tab>
                 <Tab title="Create"><Create/></Tab>
             </Tabs>
+            </Div>
         );
     }
 }
